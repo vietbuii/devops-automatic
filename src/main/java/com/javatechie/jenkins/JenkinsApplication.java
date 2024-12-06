2ab6462b0618e8e88d5aa8fdfe048a6c5b13608f
vietbuii <vietbh.26@gmail.com>
2024-12-06 12:30:13 +0700

    first commit

---
+++ src/main/java/com/javatechie/jenkins/JenkinsApplication.java
@@ -0,0 +1,21 @@
+package com.javatechie.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//echo "# devops-automatic" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/vietbuii/devops-automatic.git
+//git push -u origin main
+//@SpringBootApplication
+
+public class JenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsApplication.class, args);
+	}
+
+}
